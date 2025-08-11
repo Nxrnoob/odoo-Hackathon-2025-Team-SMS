a48a37e6bc1c7a36f42c2b7d7a337ccbd78f75f6
Nxrnoob <144538333+Nxrnoob@users.noreply.github.com>
2025-08-11 16:30:04 +0530

    Delete globetrotter-app directory

--- globetrotter-app/app/src/app/(auth)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthForm } from '@/components/auth/AuthForm';
-
-export default function LoginPage() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <AuthForm mode="login" />
-    </div>
-  );
-}
--- globetrotter-app/app/src/app/(auth)/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthForm } from '@/components/auth/AuthForm';
-
-export default function SignupPage() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <AuthForm mode="signup" />
-    </div>
-  );
-}
